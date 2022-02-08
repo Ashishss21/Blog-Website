46be2853fe96ec14b3c6934156022710624c3797
ashishss21 <ashish.sengar18@yahoo.com>
2022-02-08 12:37:10 +0530

    Sanity SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
-import Head from 'next/head';
-import Header from '../Components/Header';
-import Poster from '../Components/Poster';
-import Posts from '../Components/Posts';
-import { sanityClient, urlFor } from '../sanity';
+import Head from 'next/head'
+import Header from '../Components/Header'
+import Poster from '../Components/Poster'
+import Posts from '../Components/Posts'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-export default function Home() {
+interface Props{
+  posts:[Post];
+}
+
+export default function Home({ posts}: Props) {
+  console.log(posts);
   return (
     <div className="mx-auto">
       <Head>
         <title>Blog Website</title>
-        <link rel="icon" href="https://icon2.cleanpng.com/20180328/tiw/kisspng-medium-logo-publishing-blog-i-5abb6adc86a881.6127661915222320285516.jpg" />
+        <link
+          rel="icon"
+          href="https://icon2.cleanpng.com/20180328/tiw/kisspng-medium-logo-publishing-blog-i-5abb6adc86a881.6127661915222320285516.jpg"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
@@ -18,13 +27,30 @@ export default function Home() {
         />
       </Head>
       <Header />
-      <Poster/>
-      <Posts/>
+      <Poster />
+      <Posts />
     </div>
   )
 }
 
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author =>{
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`
 
-export const getServerSideProps = async () =>{
-  const query = `*[_type == "post]{}`;
+  const posts = await sanityClient.fetch(query);
+
+  return{
+    props:{
+      posts,
+    }
+  }
 }
